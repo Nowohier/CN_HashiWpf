e7b60c56aa44d9a10deb24f3023125ab33781e94
Christopher Nowottny <mail@chrisnowottny.com>
2025-04-01 15:04:46 +0200

    Removed HashiGeneratorCaller class

--- CN_Hashi_Generator/HashiGenerator.cs
+++ CN_Hashi_Generator/HashiGenerator.cs
@@ -14,6 +14,92 @@ namespace CNHashiGenerator
         private readonly List<Island> islands = new();
         private readonly List<Bridge> bridges = new();
 
+        public int[][] GenerateHash(int difficulty = -1, int amountNodes = 10, int width = 0, int length = 0, int alpha = 0, int beta = 0)
+        {
+            // ToDo: remove
+            var rdm = new Random();
+
+            if (difficulty >= 0)
+            {
+                if (difficulty == 0)
+                {
+                    var sizeLength = rdm.Next(5, 10);
+                    var sizeWidth = rdm.Next(5, 10);
+                    var n = (int)Math.Round(sizeWidth * sizeLength / 4.0);
+                    return GenerateHash(n, sizeLength, sizeWidth, 0, 0, false);
+                }
+                if (difficulty == 1)
+                {
+                    var sizeLength = rdm.Next(14, 16);
+                    var sizeWidth = rdm.Next(14, 16);
+                    var n = (int)Math.Round(sizeWidth * sizeLength / 4.0);
+                    return GenerateHash(n, sizeLength, sizeWidth, 1, 0, false);
+                }
+                if (difficulty == 2)
+                {
+                    var sizeLength = rdm.Next(10, 16);
+                    var sizeWidth = rdm.Next(10, 16);
+                    var n = (int)Math.Round(sizeWidth * sizeLength / 3.0);
+                    return GenerateHash(n, sizeLength, sizeWidth, 2, 0, false);
+                }
+                if (difficulty == 3)
+                {
+                    var sizeLength = rdm.Next(11, 18);
+                    var sizeWidth = rdm.Next(11, 18);
+                    var n = (int)Math.Round(sizeWidth * sizeLength / 3.0);
+                    return GenerateHash(n, sizeLength, sizeWidth, 3, 0, false);
+                }
+                if (difficulty == 4)
+                {
+                    var sizeLength = rdm.Next(10, 18);
+                    var sizeWidth = rdm.Next(10, 18);
+                    var n = (int)Math.Round(sizeWidth * sizeLength / 3.0);
+                    return GenerateHash(n, sizeLength, sizeWidth, 4, 0, false);
+                }
+                if (difficulty == 5)
+                {
+                    var sizeLength = rdm.Next(13, 18);
+                    var sizeWidth = rdm.Next(13, 18);
+                    var n = (int)Math.Round(sizeWidth * sizeLength / 3.0);
+                    return GenerateHash(n, sizeLength, sizeWidth, 5, 0, false);
+                }
+                if (difficulty == 6)
+                {
+                    var sizeLength = rdm.Next(15, 20);
+                    var sizeWidth = rdm.Next(15, 20);
+                    var n = (int)Math.Round(sizeWidth * sizeLength / 3.0);
+                    return GenerateHash(n, sizeLength, sizeWidth, 6, 0, false);
+                }
+                if (difficulty == 7)
+                {
+                    var sizeLength = rdm.Next(14, 20);
+                    var sizeWidth = rdm.Next(14, 20);
+                    var n = (int)Math.Round(sizeWidth * sizeLength / 3.0);
+                    return GenerateHash(n, sizeLength, sizeWidth, 7, 0, false);
+                }
+                if (difficulty == 8)
+                {
+                    var sizeLength = rdm.Next(16, 31);
+                    var sizeWidth = rdm.Next(16, 31);
+                    var n = (int)Math.Round(sizeWidth * sizeLength / 3.0);
+                    return GenerateHash(n, sizeLength, sizeWidth, 8, 0, false);
+                }
+                if (difficulty == 9)
+                {
+                    var sizeLength = rdm.Next(20, 31);
+                    var sizeWidth = rdm.Next(20, 31);
+                    var n = (int)Math.Round(sizeWidth * sizeLength / 3.0);
+                    return GenerateHash(n, sizeLength, sizeWidth, 9, 0, false);
+                }
+
+                throw new ArgumentException("Invalid difficulty level.");
+            }
+            else
+            {
+                return GenerateHash(amountNodes, length, width, alpha, beta, true);
+            }
+        }
+
         /// <summary>
         /// Generates a Hashi field.
         /// </summary>
@@ -24,7 +110,7 @@ namespace CNHashiGenerator
         /// <param name="beta">The beta value.</param>
         /// <param name="checkDifficulty">Determines if the difficulty should be checked.</param>
         /// <returns>a valid hashi field array with one possible solution.</returns>
-        public int[][] GenerateHash(int numberOfIslands, int sizeLength, int sizeWidth, int difficulty, int beta, bool checkDifficulty)
+        private int[][] GenerateHash(int numberOfIslands, int sizeLength, int sizeWidth, int difficulty, int beta, bool checkDifficulty)
         {
             var field = CreateHash(numberOfIslands, sizeLength, sizeWidth, difficulty, beta, checkDifficulty);
             var linear = new LinearSolutionSolverWithIterativ();
@@ -34,7 +120,7 @@ namespace CNHashiGenerator
             }
 
             Debug.WriteLine(string.Empty);
-            Debug.WriteLine(string.Join("\numberOfIslands", field.Select(row => $"{{{string.Join(", ", row)}}}")));
+            Debug.WriteLine(string.Join("\nNumberOfIslands", field.Select(row => $"{{{string.Join(", ", row)}}}")));
 
             return field;
         }
