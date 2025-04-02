277deb01327d5f6d480c2be38bbab38cde60fc04
Christopher Nowottny <mail@chrisnowottny.com>
2025-04-02 14:57:50 +0200

    Removed V1 and V2, Added first ViewModels and Commands. Drag&Drop handling still missing

---
+++ CN_HashiWpf/ViewModels/IslandViewModel.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace CNHashiWpf.ViewModels
+{
+    public class IslandViewModel : BaseViewModel
+    {
+        public IslandViewModel(int x, int y, int value)
+        {
+            X = x;
+            Y = y;
+            Value = value;
+
+            DragEnterCommand = new RelayCommand<object>(DragEnterCommandExecute, DragEnterCommandCanExecute);
+            DropCommand = new RelayCommand<object>(DropCommandExecute, DropCommandCanExecute);
+        }
+
+        public ICommand DragEnterCommand { get; }
+
+        public ICommand DropCommand { get; }
+
+        public int Value { get; set; }
+
+        public int Y { get; set; }
+
+        public int X { get; set; }
+
+        public void DragEnterCommandExecute(object? eventArgs)
+        {
+
+        }
+
+        public bool DragEnterCommandCanExecute(object? eventArgs)
+        {
+            return true;
+        }
+
+        public void DropCommandExecute(object? eventArgs)
+        {
+
+        }
+
+        public bool DropCommandCanExecute(object? eventArgs)
+        {
+            return true;
+        }
+    }
+}
