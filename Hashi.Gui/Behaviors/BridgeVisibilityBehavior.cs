596d128f4eda62110e1cbe830f069aa13305cb76
Christopher Nowottny <Chris@chrisnowottny.onmicrosoft.com>
2025-04-09 12:02:45 +0200

    Added more rules, added popup with hint message; also hint connections are highlighted now until the popup is closed

--- Hashi.Gui/Behaviors/BridgeVisibilityBehavior.cs
+++ Hashi.Gui/Behaviors/BridgeVisibilityBehavior.cs
@@ -1,10 +1,15 @@
-﻿using Hashi.Gui.Enums;
+﻿using CommunityToolkit.Mvvm.Messaging;
+using Hashi.Gui.Enums;
 using Hashi.Gui.Interfaces.Models;
 using Hashi.Gui.Interfaces.ViewModels;
+using Hashi.Gui.Messages;
 using Microsoft.Xaml.Behaviors;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
 using System.Windows.Shapes;
 
 namespace Hashi.Gui.Behaviors
@@ -12,8 +17,24 @@ namespace Hashi.Gui.Behaviors
     /// <summary>
     ///      A behavior that controls the visibility of a bridge based on the number of connections in a Hashi game.
     /// </summary>
-    public class BridgeVisibilityBehavior : Behavior<Line>
+    public class BridgeVisibilityBehavior : Behavior<Line>, IRecipient<HintPopupClosedMessage>
     {
+        private readonly DoubleAnimation fadeInAnimation = new()
+        {
+            From = 0.0,
+            To = 1.0,
+            Duration = TimeSpan.FromSeconds(0.5),
+            EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+        };
+
+        private readonly ColorAnimation fadeOutAnimation = new()
+        {
+            From = (Color)ColorConverter.ConvertFromString("#3abf5b"),
+            To = Colors.Black,
+            Duration = TimeSpan.FromSeconds(0.5),
+            EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+        };
+
         /// <summary>
         ///       Identifies the <see cref="AllConnections"/> dependency property.
         /// </summary>
@@ -92,13 +113,18 @@ namespace Hashi.Gui.Behaviors
         protected override void OnAttached()
         {
             base.OnAttached();
+
+            AssociatedObject.IsVisibleChanged += AssociatedObject_IsVisibleChanged;
             UpdateVisibility();
+
+            WeakReferenceMessenger.Default.Register(this);
         }
 
         /// <inheritdoc />
         protected override void OnDetaching()
         {
             base.OnDetaching();
+            AssociatedObject.IsVisibleChanged -= AssociatedObject_IsVisibleChanged;
             AllConnections.CollectionChanged -= OnCollectionChanged!;
         }
 
@@ -125,6 +151,43 @@ namespace Hashi.Gui.Behaviors
                 BridgeTypeEnum.VerticalDoubleDown => island.BridgesDown.Count == 2 ? Visibility.Visible : Visibility.Hidden,
                 _ => Visibility.Hidden
             };
+        }
+
+        private void AssociatedObject_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (AssociatedObject is not { } line || AssociatedObject.DataContext is not IIslandViewModel island) return;
+
+            var runFadeInAnimation = BridgeType switch
+            {
+                BridgeTypeEnum.Horizontal => island.BridgesLeft.Count == 1 && island.BridgesRight.Count == 1 && (island.BridgesLeft.First().IsHint || island.BridgesRight.First().IsHint),
+                BridgeTypeEnum.HorizontalDouble => island.BridgesLeft.Count == 2 && island.BridgesRight.Count == 2,
+                BridgeTypeEnum.Vertical => island.BridgesUp.Count == 1 && island.BridgesDown.Count == 1 && (island.BridgesUp.First().IsHint || island.BridgesDown.First().IsHint),
+                BridgeTypeEnum.VerticalDouble => island.BridgesUp.Count == 2 && island.BridgesDown.Count == 2,
+                BridgeTypeEnum.HorizontalLeft => island.BridgesLeft.Count == 1 && island.BridgesLeft.First().IsHint,
+                BridgeTypeEnum.HorizontalDoubleLeft => island.BridgesLeft.Count == 2,
+                BridgeTypeEnum.HorizontalRight => island.BridgesRight.Count == 1 && island.BridgesRight.First().IsHint,
+                BridgeTypeEnum.HorizontalDoubleRight => island.BridgesRight.Count == 2,
+                BridgeTypeEnum.VerticalUp => island.BridgesUp.Count == 1 && island.BridgesUp.First().IsHint,
+                BridgeTypeEnum.VerticalDoubleUp => island.BridgesUp.Count == 2,
+                BridgeTypeEnum.VerticalDown => island.BridgesDown.Count == 1 && island.BridgesDown.First().IsHint,
+                BridgeTypeEnum.VerticalDoubleDown => island.BridgesDown.Count == 2,
+                _ => false
+            };
+
+            if (line.Visibility == Visibility.Visible && runFadeInAnimation)
+            {
+                line.Stroke = (SolidColorBrush)new BrushConverter().ConvertFrom("#3abf5b")!;
+                line.Effect = new BlurEffect { Radius = 10 };
+                line.BeginAnimation(UIElement.OpacityProperty, fadeInAnimation);
+            }
+        }
+
+        public void Receive(HintPopupClosedMessage message)
+        {
+            if (AssociatedObject is not { Stroke: SolidColorBrush solidColorBrush } line || line.Effect == null) return;
+            solidColorBrush.BeginAnimation(SolidColorBrush.ColorProperty, fadeOutAnimation);
+            line.Effect = null;
+            line.StrokeThickness = 1;
         }
     }
 }
--- Hashi.Rules/TwoConnections/TwoConnectionsRule2.cs
+++ Hashi.Rules/TwoConnections/TwoConnectionsRule2.cs
@@ -5,12 +5,14 @@ namespace Hashi.Rules.TwoConnections
 {
     public class TwoConnectionsRule2 : BaseRule
     {
+        protected override string RuleMessage => "Islands with a maximum of two bridges can be connected to another island if there is only one neighbor still accepting connections.";
+
         /// <inheritdoc />
         public override void Define()
         {
             IIslandViewModel island = default!;
             IIslandViewModel neighbor = default!;
-            List<IIslandViewModel> allNeighbors = default!;
+            List<IIslandViewModel> allValidNeighbors = default!;
             IConnectionManagerViewModel connectionManager = default!;
             var allNeighborsCount = 0;
             var missingConnectionsCount = 0;
@@ -18,14 +20,14 @@ namespace Hashi.Rules.TwoConnections
             When()
                 .Match(() => island, x => x.MaxConnectionsReached == false && x.MaxConnections == 2)
                 .Query(() => connectionManager, q => q.Match<IConnectionManagerViewModel>())
-                .Let(() => allNeighbors, () => island.GetAllVisibleNeighbors())
-                .Let(() => neighbor, () => allNeighbors.First())
-                .Let(() => allNeighborsCount, () => allNeighbors.Count)
+                .Let(() => allValidNeighbors, () => island.GetAllVisibleNeighbors().Where(x => x.MaxConnectionsReached == false).ToList())
+                .Let(() => neighbor, () => allValidNeighbors.First())
+                .Let(() => allNeighborsCount, () => allValidNeighbors.Count)
                 .Let(() => missingConnectionsCount, () => neighbor.MaxConnections - neighbor.AllConnections.Count)
                 .Having(() => allNeighborsCount == 1);
 
             Then()
-                .Do(ctx => AddConnectionsToOneTarget(island, allNeighbors.First(), missingConnectionsCount, connectionManager));
+                .Do(ctx => AddConnectionsToOneTarget(island, allValidNeighbors.First(), missingConnectionsCount, connectionManager));
         }
     }
 }
