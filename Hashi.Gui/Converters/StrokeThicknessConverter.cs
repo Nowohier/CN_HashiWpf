6615e18b5ae7a0b09a9c63feecce5df5ce9ea140
Christopher Nowottny <Chris@chrisnowottny.onmicrosoft.com>
2025-04-12 11:38:21 +0200

    Fixed stroke thickness bug

---
+++ Hashi.Gui/Converters/StrokeThicknessConverter.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Hashi.Gui.Converters
+{
+    /// <summary>
+    /// A converter that returns a <see cref="Thickness"/> based on the size of the <see cref="ItemsControl"/>.
+    /// This fixes a WPF issue that shapes are not rendered correctly when the size is too small.
+    /// </summary>
+    public class StrokeThicknessConverter : IValueConverter
+    {
+        /// <inheritdoc />
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is not ItemsControl itemsControl)
+            {
+                return 1;
+            }
+
+            if (itemsControl.ActualWidth > 1000 || itemsControl.ActualHeight > 1000)
+            {
+                return 1.5;
+            }
+
+            return 1;
+        }
+
+        /// <inheritdoc />
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
