b221d6783233c260762ad1c4455abdf07d9a6ead
Christopher Nowottny <mail@chrisnowottny.com>
2025-04-17 22:41:58 +0200

    Corrected a rule definition text

--- Solver/Hashi.Rules/_5ConnectionsRule3.cs
+++ Solver/Hashi.Rules/_5ConnectionsRule3.cs
@@ -5,7 +5,7 @@ using Hashi.Gui.Translation;
 namespace Hashi.Rules;
 
 /// <summary>
-/// If a 5-connection island has four neighbors and two full neighbors have a total of no more than three connections to the island, a connection must be drawn to each of the remaining neighbors if no connection has already been established.
+/// If a 5-connection island has four neighbors and two full neighbors have a total of no more than two connections to the island, a connection must be drawn to each of the remaining neighbors if no connection has already been established.
 /// </summary>
 public class _5ConnectionsRule3(IRuleInfoProvider ruleInfoProvider, IIslandProvider islandProvider) : BaseRule(ruleInfoProvider, islandProvider)
 {
