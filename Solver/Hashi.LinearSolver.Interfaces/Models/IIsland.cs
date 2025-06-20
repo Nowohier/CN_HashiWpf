bf5461025b8e713821f13aa311a0cbeef001124a
Christopher Nowottny <mail@chrisnowottny.com>
2025-06-21 00:07:28 +0200

    removed old solver and added new one to autofac container as single instance

--- Solver/Hashi.LinearSolver.Interfaces/Models/IIsland.cs
+++ Solver/Hashi.LinearSolver.Interfaces/Models/IIsland.cs
@@ -1,78 +1,33 @@
-﻿namespace Hashi.LinearSolver.Interfaces.Models;
-
-/// <summary>
-///     Represents an island in the Hashi game.
-/// </summary>
-public interface IIsland
+﻿namespace Hashi.LinearSolver.Interfaces.Models
 {
     /// <summary>
-    ///     Gets the y coordinate.
+    /// Represents an island in the Hashiwokakero puzzle.
     /// </summary>
-    int Y { get; }
+    public interface IIsland
+    {
+        /// <summary>
+        /// Gets the unique identifier for the island.
+        /// </summary>
+        int Id { get; }
 
-    /// <summary>
-    ///     Gets the x coordinate.
-    /// </summary>
-    int X { get; }
+        /// <summary>
+        /// Gets the row index of the island in the puzzle grid.
+        /// </summary>
+        int Row { get; }
 
-    /// <summary>
-    ///     Gets the actual value of the island.
-    /// </summary>
-    int Value { get; set; }
+        /// <summary>
+        /// Gets the column index of the island in the puzzle grid.
+        /// </summary>
+        int Col { get; }
 
-    /// <summary>
-    ///     Gets the upper neighbor.
-    /// </summary>
-    IIsland? Up { get; }
+        /// <summary>
+        /// Gets the number of bridges required to connect this island to others.
+        /// </summary>
+        int BridgesRequired { get; }
 
-    /// <summary>
-    ///     Gets the lower neighbor.
-    /// </summary>
-    IIsland? Down { get; }
-
-    /// <summary>
-    ///     Gets the right neighbor.
-    /// </summary>
-    IIsland? Right { get; }
-
-    /// <summary>
-    ///     Gets the left neighbor.
-    /// </summary>
-    IIsland? Left { get; }
-
-    /// <summary>
-    ///     Gets the island number.
-    /// </summary>
-    int Number { get; }
-
-    /// <summary>
-    ///     Gets the lower neighbors.
-    /// </summary>
-    List<IIsland> LowerNeighbors { get; }
-
-    /// <summary>
-    ///     Gets the upper neighbors.
-    /// </summary>
-    List<IIsland> UpNeighbors { get; }
-
-    /// <summary>
-    ///     Adds a bridge to the island.
-    /// </summary>
-    /// <param name="field">The array of fields.</param>
-    /// <param name="islands">The list of islands.</param>
-    void SetAllNeighbors(int[][] field, List<IIsland> islands);
-
-    /// <summary>
-    ///     Returns all islands between this island and the lower neighbor.
-    /// </summary>
-    /// <param name="island">A list of all islands.</param>
-    /// <returns>all islands between this island and the lower neighbor.</returns>
-    List<IIsland> DownBlocked(List<IIsland> island);
-
-    /// <summary>
-    ///     Returns all islands between this island and the right neighbor.
-    /// </summary>
-    /// <param name="island">A list of all islands.</param>
-    /// <returns>all islands between this island and the right neighbor.</returns>
-    List<IIsland> RightBlocked(List<IIsland> island);
+        /// <summary>
+        /// Gets the list of neighboring islands' IDs that this island is connected to.
+        /// </summary>
+        List<int> Neighbors { get; }
+    }
 }
